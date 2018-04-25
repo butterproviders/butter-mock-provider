89c8e77a900acb96bbcf45934a32c7dbc5fa45de
Niv Sardi <xaiki@evilgiggle.com>
2018-04-24 21:58:00 -0300

    base mock provider
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Provider = require('butter-provider')
+const ButterProvider = require('butter-provider')
 
 const defaultConfig = {
     name: 'mock',
@@ -17,26 +17,47 @@ const defaultConfig = {
 
 // if you feel bad for your items I feel bad for you son,
 // I got 99 problems but generating generic data aint one.
-const mockData = Array.from(Array[99]).reduce((a, c ,i) => (
+const mockData = Array.from(Array(99)).reduce((a, c ,i) => (
     Object.assign(a, {
         [i]: {
             id: i,
+            type: ButterProvider.ItemType.MOVIE,
             title: `mock ${i}`,
             sinopsis: `the marvelous adventure of the big break of mock ${i}`,
+            year: 1970 + i,
+            rating: i % 9,
+            backdrop: `http://imgur.com/${i}`,
+            poster: `http://imgur.com/${i*i}`,
+            subtitle: [],
+            trailer: `http://ytu.be/${i}`,
+            torrents: {
+                '720p': {
+                    url: `https://tpb.me/${i}`,
+                    size: 1024*i
+                },
+                '1080p': {
+                    url: `https://tpb.me/${i}-big`,
+                    size: 1024*1024*i
+                },
+                '4k': {
+                    url: `https://tpb.me/${i}-very-big`,
+                    size: 1024*1024*1024*i
+                }
+            },
             runtime: 100 + i,
             genres: [`${i}ism`]
         }
     })
-))
+), {})
 
 module.exports = class MockProvider extends ButterProvider {
-    constructor (args, config= defaultconfig) {
+    constructor (args, config= defaultConfig) {
         super(args, config)
     }
 
     fetch() {
         return Promise.resolve({
-            results: [...mockData],
+            results: [Object.values(mockData)],
             hasMore: false
         })
     }
